8401237a3a55e7be2cda3721238e7996aeaa6b23
Evaristo S. Rodrigues <evaristosrodrigues@pop-os.localdomain>
2019-12-08 23:54:51 -0300

    Updates

---
+++ precache-manifest.90b51ed4dc5f0fd9f20daa16d980cbc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d77dd66d114cf441bd0fb70937bdf8e",
+    "url": "/RoboFriends-ServiceWorker/index.html"
+  },
+  {
+    "revision": "908bcd4f973780e5c9fd",
+    "url": "/RoboFriends-ServiceWorker/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "d34ac73ccb5825e5bd31",
+    "url": "/RoboFriends-ServiceWorker/static/css/main.0eae2450.chunk.css"
+  },
+  {
+    "revision": "908bcd4f973780e5c9fd",
+    "url": "/RoboFriends-ServiceWorker/static/js/2.ede84d3b.chunk.js"
+  },
+  {
+    "revision": "d34ac73ccb5825e5bd31",
+    "url": "/RoboFriends-ServiceWorker/static/js/main.49b363f6.chunk.js"
+  },
+  {
+    "revision": "ebc5f278edc14a1c5c92",
+    "url": "/RoboFriends-ServiceWorker/static/js/runtime~main.6b51bd7b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboFriends-ServiceWorker/static/media/SEGA.12a3db8b.woff"
+  }
+]);
