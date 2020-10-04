98e12899406debaf7dd3f70e1c1ec459d5802db7
Evaristo S. Rodrigues <evaristosrodrigues@pop-os.localdomain>
2020-10-04 17:15:07 -0300

    update

--- src/component/CounterButton.js
+++ src/component/CounterButton.js
@@ -24,7 +24,7 @@ class CounterButton extends PureComponent{
 
     render(){
         console.log('CounterButton');
-        return (<button color={this.props.color} onClick={this.updateCount}> Count:{this.state.count}</button>);
+        return (<button color={this.props.color} onClick={this.updateCount}> Count: {this.state.count} </button>);
     }
 
 }
